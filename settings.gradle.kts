e88b5bb71286989e7c941a31790f83c071cf9cb1
Florian D <delarue.florian@gmail.com>
2026-06-30 08:29:35 +0200

    Add Gradle settings

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CoachApp"
+include(":app")
+include(":wear")
+include(":shared")
